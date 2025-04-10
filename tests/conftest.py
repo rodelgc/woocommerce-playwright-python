521eed825f879af42d6b72fd733877cacfe92072
Rodel Calasagsag <od.rodel.calasagsag@gmail.com>
2025-04-10 11:40:58 +0800

    Set up and tear down authenticated contexts per function

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,7 +1,7 @@
 import re
 import os
 import pytest
-from playwright.sync_api import Browser, BrowserContext, expect
+from playwright.sync_api import Browser, expect, StorageState
 from dotenv import load_dotenv
 
 # Load environment variables
@@ -11,13 +11,9 @@ WORDPRESS_PASSWORD = os.getenv("WORDPRESS_PASSWORD")
 WORDPRESS_CUSTOMER_USERNAME = os.getenv("WORDPRESS_CUSTOMER_USERNAME")
 WORDPRESS_CUSTOMER_PASSWORD = os.getenv("WORDPRESS_CUSTOMER_PASSWORD")
 
-# Storage state paths
-MERCHANT_STORAGE_STATE_PATH = "playwright/.auth/merchant.state.json"
-CUSTOMER_STORAGE_STATE_PATH = "playwright/.auth/customer.state.json"
-
 
 @pytest.fixture(autouse=True, scope="session")
-def merchant_login(browser: Browser, base_url: str):
+def merchant_storage_state(browser: Browser, base_url: str):
     # Setup: log in as merchant and save storage state
     context = browser.new_context(base_url=base_url)
     page = context.new_page()
@@ -27,20 +23,18 @@ def merchant_login(browser: Browser, base_url: str):
     page.get_by_label("Password", exact=True).fill(WORDPRESS_PASSWORD)
     page.get_by_role("button", name="Log In").click()
     expect(page.get_by_role("menuitem", name="Howdy")).to_be_visible()
-    merchant_storage_state = context.storage_state(path=MERCHANT_STORAGE_STATE_PATH)
-    merchant_context = browser.new_context(
-        base_url=base_url, storage_state=merchant_storage_state
+    merchant_storage_state = context.storage_state(
+        path="playwright/.auth/merchant.state.json"
     )
 
-    yield merchant_context
+    yield merchant_storage_state
 
-    # Teardown: close all created contexts
-    merchant_context.close()
+    # Teardown: close unauthenticated context
     context.close()
 
 
 @pytest.fixture(autouse=True, scope="session")
-def customer_login(browser: Browser, base_url: str):
+def customer_storage_state(browser: Browser, base_url: str):
     # Setup: log in as customer and save storage state
     context = browser.new_context(base_url=base_url)
     page = context.new_page()
@@ -52,23 +46,45 @@ def customer_login(browser: Browser, base_url: str):
     page.get_by_role("button", name="Log in").click()
     expect(page.get_by_role("heading", name="My account")).to_be_visible()
     expect(page.get_by_text(f"Hello {WORDPRESS_CUSTOMER_USERNAME}")).to_be_visible()
-    customer_storage_state = context.storage_state(path=CUSTOMER_STORAGE_STATE_PATH)
-    customer_context = browser.new_context(
-        base_url=base_url, storage_state=customer_storage_state
+    customer_storage_state = context.storage_state(
+        path="playwright/.auth/customer.state.json"
     )
 
-    yield customer_context
+    yield customer_storage_state
 
-    # Teardown: close all created contexts
-    customer_context.close()
+    # Teardown: close unauthenticated context
     context.close()
 
 
 @pytest.fixture(scope="function")
-def merchant_page(merchant_login: BrowserContext):
-    return merchant_login.new_page()
+def merchant_page(
+    merchant_storage_state: StorageState, browser: Browser, base_url: str
+):
+    # Setup: create authenticated merchant context
+    merchant_context = browser.new_context(
+        base_url=base_url,
+        storage_state=merchant_storage_state,
+    )
+    merchant_page = merchant_context.new_page()
+
+    yield merchant_page
+
+    # Teardown: close authenticated context
+    merchant_context.close()
 
 
 @pytest.fixture(scope="function")
-def customer_page(customer_login: BrowserContext):
-    return customer_login.new_page()
+def customer_page(
+    customer_storage_state: StorageState, browser: Browser, base_url: str
+):
+    # Setup: create authenticated customer context
+    customer_context = browser.new_context(
+        base_url=base_url,
+        storage_state=customer_storage_state,
+    )
+    customer_page = customer_context.new_page()
+
+    yield customer_page
+
+    # Teardown: close authenticated context
+    customer_context.close()
