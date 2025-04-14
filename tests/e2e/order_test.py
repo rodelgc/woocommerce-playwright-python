04b4066248a644ab0fab120a95d36f3cea7c7718
Rodel Calasagsag <od.rodel.calasagsag@gmail.com>
2025-04-14 13:42:24 +0800

    Add readability comments

--- tests/e2e/order_test.py
+++ tests/e2e/order_test.py
@@ -15,8 +15,10 @@ def test_customer_can_add_product_to_cart(
     expected_item_total = "89.94"
 
     def goto_product_page_and_add_to_cart() -> CartPage:
+        # Product data
         product_url = product["permalink"]
 
+        # User actions
         product_page = ProductPage(customer_page)
         product_page.goto(product_url)
         product_page.fill_quantity(expected_quantity)
@@ -25,9 +27,11 @@ def test_customer_can_add_product_to_cart(
         return cart_page
 
     def check_cart_details(cart_page: CartPage) -> None:
+        # Product data
         product_name = product["name"]
         product_regular_price = product["regular_price"]
 
+        # User actions
         cart_item = cart_page.get_cart_item_by_product_name(product_name)
         cart_item.has_expected_product_link(product_name)
         cart_item.has_expected_quantity(expected_quantity)
