c3fcbc07b85716055383cdccda55070e83c08c3d
Rodel Calasagsag <od.rodel.calasagsag@gmail.com>
2025-04-14 11:58:04 +0800

    Create customer fixture

---
+++ tests/_fixtures/customer_fixtures.py
@@ -0,0 +1,57 @@
+from dotenv import load_dotenv
+import pytest
+import os
+
+from playwright.sync_api import APIRequestContext
+
+# Load environment variables
+load_dotenv("local.env")
+WORDPRESS_CUSTOMER_EMAIL = os.getenv("WORDPRESS_CUSTOMER_EMAIL")
+
+
+@pytest.fixture(scope="session")
+def customer(request_context: APIRequestContext):
+    data = {
+        "billing": {
+            "first_name": "Custer",
+            "last_name": "Curry",
+            "company": "Automattic Inc.",
+            "address_1": "#60 Suite 100, Main Street",
+            "address_2": "",
+            "city": "San Francisco",
+            "postcode": "94116",
+            "country": "US",
+            "state": "CA",
+            "phone": "(415) 555-1234",
+        },
+    }
+
+    # Get customer ID by email
+    response_list_customers = request_context.get(
+        "wp-json/wc/v3/customers",
+        data={
+            "per_page": 100,
+            "email": WORDPRESS_CUSTOMER_EMAIL,
+            "_fields": ["id", "email"],
+            "role": "all",
+        },
+    )
+    print(f"Response list customers:\n{response_list_customers.json()}")
+    assert response_list_customers.ok
+    customer_matches = response_list_customers.json()
+    assert (
+        len(customer_matches) == 1
+    ), f"No customer found with the specified email: {WORDPRESS_CUSTOMER_EMAIL}"
+
+    customer_matches: list = response_list_customers.json()
+    customer_id = customer_matches[0]["id"]
+
+    # Update customer data
+    response_update_customer = request_context.put(
+        f"/wp-json/wc/v3/customers/{customer_id}",
+        data=data,
+    )
+    assert response_update_customer.ok
+    customer = response_update_customer.json()
+
+    yield customer
--- tests/conftest.py
+++ tests/conftest.py
@@ -12,6 +12,9 @@ from tests._fixtures.request_fixtures import (
 from tests._fixtures.cleanup_fixtures import (
     cleanup_store,
 )
+from tests._fixtures.customer_fixtures import (
+    customer,
+)
 from tests._fixtures.store_settings_fixtures import (
     reset_merchant_settings,
 )
